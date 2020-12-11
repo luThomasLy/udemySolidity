021bb98d2c46a8bb1b797d083df2ec685b363d1c
lu <luthomasly@gmail.com>
2020-12-11 16:07:42 +0100

    test push

---
+++ 02 MyFirstContact.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.6.2;
+
+contract MyFirstContract {
+    int public age = 38;
+    uint public nbOfChicken = 45;
+    string /*memory*/ public firstName = "TL";
+    bool public vegetarian = false;
+    address myAddress = 0x5ABf6C25F74Eb8843763da773190AA32778356F9;
+}
